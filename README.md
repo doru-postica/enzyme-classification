a4c8ba34262316280fcbce4e7e4525c9e69d40de
Doru Postica <doru.postica@gmail.com>
2023-07-11 18:26:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Enzyme classification project
+***
+Given 31 features of an observation, predict probability that it has enzymes EC1 and EC2. (multi label binary classification)
+
+This project is based on Kaggle challenge available [here](https://www.kaggle.com/competitions/playground-series-s3e18/overview).
+
+###  📚 **Data preprocessing and model choice.**
+* All features were numerical, used a StandardScaler.
+* Feature engineering attempts did not yield a better result. (decrease result by 0.003)
+* Inspected box plots of each feature and removed 2.5% of samples. (LocalOutlierFactor, capping did not yield good results) (this increased result by 0.004)
+* Used Microsoft's FLAML AutoML library to check feature space of 4 models: XGBoost, Random Forest, L1 Logistic Regression, L2 Logistic Regression.
+* XGBoost was chosen.
+
+*Working file and training data can be found in "eda" folder.*
+
+### ⭐️ Performance
+* Kaggle metric was mean ROC AUC for the 2 labels
+  * Training data: 0.6456
+  * Test data: 0.6495
+  * Kaggle personal submission: 0.6514
+  * Kaggle top result: 0.66242
+  * Leaderboard: 376/1056
+
+### 🚀 Model deployment
+* Deployed on Streamlit, live version can be found [here](TBD).
+* Given an input file, model can run predictions for both labels and provide a download file.
+  * *A sample file is also provided*
---
+++ streamlit_app.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import streamlit as st
+import joblib
+import sys
+
+sys.tracebacklimit = 0
+
+
+def load_models():
+    EC1_model = joblib.load('fit_pickles/fit_EC1_model.pkl')
+    EC2_model = joblib.load('fit_pickles/fit_EC2_model.pkl')
+    scaler = joblib.load('fit_pickles/fit_scaler.pkl')
+    feature_names = joblib.load('fit_pickles/feature_names.pkl')
+    return (EC1_model, EC2_model, scaler, feature_names)
+
+
+def display_greetings():
+    st.title('Enzyme Classification Model')
+    st.image('https://i.kym-cdn.com/photos/images/newsfeed/000/234/739/fa5.jpg')
+    st.header('Please upload your file to generate prediction:')
+
+
+def provide_sample_files():
+    st.text('Empty template to fill in:')
+    with open('eda/empty_template.csv', 'r') as empty_template:
+        st.download_button(label='Template', data=empty_template, mime='text/csv')
+
+    st.text('Dummy data if you want to test the model:')
+    with open('eda/test_data.csv', 'r') as dummy_data:
+        st.download_button(label='Dummy_data', data=dummy_data, mime='text/csv')
+
+
+def get_streamlit_input():
+    while True:
+        try:
+            uploaded_file = st.file_uploader(label='Upload your csv here:', type=['csv'])
+            input_dataframe = pd.read_csv(uploaded_file)
+        except Exception:
+            continue
+        else:
+            break
+    return input_dataframe
+
+
+def validate_column_names(input_dataframe):
+    if input_dataframe.drop(['id'], axis=1).columns.tolist() != feature_names:
+        raise Exception('Columns do not match our template. Please upload another file.')
+    return
+
+
+def validate_data_types(input_dataframe):
+    for column in input_dataframe.columns:
+        if input_dataframe[column].dtype == 'object':
+            raise Exception('Unexpected values found in file. Please upload another file.')
+    return
+
+
+def prepare_data(input_dataframe):
+    mypred_df = input_dataframe.drop(['id'], axis=1)
+    mypred_df = scaler.transform(mypred_df)
+    return mypred_df
+
+
+def get_output(mypred_df):
+    EC1 = EC1_model.predict_proba(mypred_df)[:, 1]
+    EC1 = pd.Series(data=EC1, name='EC1')
+
+    EC2 = EC2_model.predict_proba(mypred_df)[:, 1]
+    EC2 = pd.Series(data=EC2, name='EC2')
+
+    final_file = (pd.concat([input_dataframe, EC1, EC2], axis=1)).to_csv(index=False)
+    st.success('Success!', icon="✅")
+    st.download_button(label='Download results', data=final_file, mime='text/csv')
+
+
+# Executing script:
+
+EC1_model, EC2_model, scaler, feature_names = load_models()
+display_greetings()
+provide_sample_files()
+
+input_dataframe = get_streamlit_input()
+validate_column_names(input_dataframe)
+validate_data_types(input_dataframe)
+
+mypred_df = prepare_data(input_dataframe)
+
+if st.button('Generate results:'):
+    get_output(mypred_df)
